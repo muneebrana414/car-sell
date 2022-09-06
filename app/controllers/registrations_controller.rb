78b95d215e635437568cffbece5ff0ff384d26c7
M Muneeb <muneebrana414@gmail.com>
2022-09-06 17:21:55 +0500

    implement sign in with email or phone number

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,5 +6,6 @@ class RegistrationsController < Devise::RegistrationsController
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :username, :phone_number, :password)}
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:phone_number, :username, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:login, :password)}
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
 class User < ApplicationRecord
+  attribute :login, :string
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-  :confirmable
+  :confirmable, authentication_keys: [:login]
 
   validates :username, length: { in: 6..30 }
   validates :encrypted_password, format: { with: /\A(?=.*\d)(?=.*([A-Z]))([\x20-\x7E]|[^\x00-\x7F]){6,100}\z/ }
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(phone_number) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:phone_number) || conditions.has_key?(:email)
+      where(conditions.to_h).first 
+    end 
+  end 
 end
