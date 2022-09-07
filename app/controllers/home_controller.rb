7ebb132781b656f47503ea1b5a260a6cf73fac1a
M Muneeb <muneebrana414@gmail.com>
2022-09-07 12:52:09 +0500

    Home Page implement

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user! 
   def index
+    @filters = true
+  end
+
+  def new
+    @filters = ActiveModel::Type::Boolean.new.cast(params[:filters])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get 'home/new', to: 'home#new'
 
   devise_for :users, controllers: {registrations: "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
